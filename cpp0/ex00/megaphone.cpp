4ebbd1848b93c44d8c18e88846e51fca39c622f4
burcakseven <burcakseven123@gmail.com>
2023-05-16 19:36:24 +0300

    cpp0/ex00 seems like done

--- cpp0/ex00/megaphone.cpp
+++ cpp0/ex00/megaphone.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
+#include <cctype>
+
+void upperString(char *str)
+{
+    while (*str != '\0')
+    {
+        std::cout << static_cast<char>(toupper(*str));
+        str++;
+    }
+}
 
 int main(int argc, char **argv)
 {
     if(argc <= 1)
     {
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+        return 1;
     }
+    while (*(++argv) != '\0')
+    {
+        upperString(*argv);
+        if(*(argv+1) != '\0')
+            std::cout << " ";
+    }
+    
     return 0;
 }
