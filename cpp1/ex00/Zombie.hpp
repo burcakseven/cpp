8b858b3ce217f9c4dc5528d62b96b9dc80c853f2
burcakseven <burcakseven123@gmail.com>
2023-06-09 18:14:49 +0300

    creating newZombies

---
+++ cpp1/ex00/Zombie.hpp
@@ -0,0 +1,16 @@
+#ifndef ZOMBIE_H
+#define ZOMBIE_H
+#include <iostream>
+
+class Zombie
+{
+private:
+    std::string name;
+public:
+    Zombie(const std::string name);
+    void announce(void);
+};
+
+Zombie* newZombie(std::string name);
+
+#endif
---
+++ cpp1/ex00/main.cpp
@@ -0,0 +1,7 @@
+#include "Zombie.hpp"
+
+int main()
+{
+    newZombie("hatice");
+    return 0;
+}
